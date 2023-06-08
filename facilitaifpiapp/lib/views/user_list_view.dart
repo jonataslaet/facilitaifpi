df958823a77db1c5c6e847bbee0ffd4610d541c0
jonataslaet <jonataslaetprogramador@gmail.com>
2023-06-08 09:53:13 -0300

    app-release-0.0.1: Created IconButton and TextButton
    IconButton is for updating user list
    TextButton is for backing to login page

--- facilitaifpiapp/lib/views/user_list_view.dart
+++ facilitaifpiapp/lib/views/user_list_view.dart
@@ -11,13 +11,29 @@ class UserListView extends StatelessWidget {
         title: const Text("Contatos"),
         actions: [
           IconButton(
-            onPressed: ()=> Navigator.pushNamed(context, '/users'),
+            onPressed: () => Navigator.pushNamed(context, '/users'),
             icon: const Icon(Icons.update),
           )
         ],
       ),
       body: const UserListController(),
+      bottomNavigationBar: Container(
+        height: 50.0,
+        color: const Color.fromARGB(238, 255, 183, 59),
+        child: Center(
+          child: TextButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/');
+            },
+            child: const Text(
+              "Voltar para a página de login",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
-
